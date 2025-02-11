da7118e7f1b1cd1a5214ea89a7b6a6e774b6fe48
rbhusare <bhusarerahul99@gmail.com>
2025-02-11 10:41:56 +0530

    basic setup

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.jetbrains.kotlin.kapt)
 }
 
 android {
@@ -59,6 +60,12 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+
+    implementation(libs.compose.nav)
+    implementation(libs.coil.kt)
+    implementation(libs.dagger.hilt)
+    kapt(libs.kapt.hilt)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
